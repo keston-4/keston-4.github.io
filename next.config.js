74e487c4b0dcc397c3701413a6dc7db5b995227f
Kurt Weston <kurt.weston97@gmail.com>
2024-11-24 20:02:01 +1300

    Update config for easy production deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+}
+
+if (isProd) {
+  nextConfig = {
+    ...nextConfig,
+    output: 'export',
+  }
 }
 
 module.exports = nextConfig
