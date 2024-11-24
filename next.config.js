529a528d8e732d262d9e6dbf979e3ea61bb46a72
Kurt Weston <kurt.weston97@gmail.com>
2024-11-24 19:56:26 +1300

    update config for export output

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
