498a163acdd8211bbf51d6486a3223b05f5bc2e2
Kurt Weston <kurt.weston97@gmail.com>
2024-11-24 20:21:00 +1300

    Update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+let nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
 if (isProd) {
+  console.log('IS PROD')
   nextConfig = {
     ...nextConfig,
     output: 'export',
   }
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
